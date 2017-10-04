a6c7d047301b6a94bc4d133758f0cf90d6c2fdcd
Ryan Manchester <ryanm17@gmail.com>
2017-10-04 20:55:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,8 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}" != "#{phrase}".upcase
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 
 end

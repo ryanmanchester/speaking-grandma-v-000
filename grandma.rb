7891f0b723b51eba17e9c7b9203a6d510cc08e0c
Ryan Manchester <ryanm17@gmail.com>
2017-10-04 20:54:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}" != "#{phrase}".upcase
-    "HUH?1 SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 
 end
